4c645f5034a7dbc896811441802e9ffbb5ccc9b4
Jeziel Oliveira de Paula <jeziel.oliveira.paula@gmail.com>
2024-08-20 15:53:48 -0300

    coloquei os comentarios na classe Main

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,60 @@ import calculo.Divisao;
 import calculo.Soma;
 import calculo.Subtracao;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int numero1;
-        int numero2;
-        numero1 = 6;
-        numero2 = 6;
+        //craindo o scanner
+        Scanner scanner = new Scanner(System.in);
 
+        //criando as variaveis para pegar os imports dos calculos
         Soma somando = new Soma();
         Subtracao subtracao = new Subtracao();
         Multiplicacao multiplicacao = new Multiplicacao();
         Divisao divisao = new Divisao();
 
-        System.out.println(somando.somando(numero1, numero2));
-        System.out.println(subtracao.subtracao(numero1, numero2));
-        System.out.println(multiplicacao.multiplicacao(numero1, numero2));
-        System.out.println(divisao.divisaoRessultado(numero1, numero2));
-        
+        //criando as variaveis
+        int numero1;
+        int numero2;
+
+        //criando as mensagens colocando para ler as variaveis
+        System.out.println(
+                "Digite o primeiro numero para calcular"
+        );
+        numero1 = scanner.nextInt();
+        System.out.println(
+                "Digite o segundo numero para calcular"
+        );
+        numero2 = scanner.nextInt();
+
+        //soma
+        System.out.println(
+                somando.somando(
+                        numero1,
+                        numero2
+                )
+        );
+        //subtração
+        System.out.println(
+                subtracao.subtracao(
+                        numero1,
+                        numero2
+                )
+        );
+        //multiplicação
+        System.out.println(
+                multiplicacao.multiplicacao(
+                        numero1,
+                        numero2
+                )
+        );
+        //divisão
+        System.out.println(
+                divisao.divisaoRessultado(
+                        numero1,
+                        numero2
+                )
+        );
     }
 }
